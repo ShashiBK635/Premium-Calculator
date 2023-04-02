3d5551729d3266037981c78aa5fae4d933d0603c
shashi635 <sik635@gmail.com>
2023-04-02 09:31:01 +0530

    initial commit

---
+++ PremiumCalculator/Controllers/PremiumController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PremiumCalculator.Models;
+using System.Collections.Generic;
+
+namespace PremiumCalculator.Controllers
+{
+    public class PremiumController : Controller
+    {
+        [Route("/")]
+        public IActionResult Calculator()
+        {
+            var model = new PremiumViewModel() { OccupationList = new List<SelectListItem>() };
+            return View(model);
+        }
+
+    }
+}
---
+++ PremiumCalculator/Models/PremiumViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+
+namespace PremiumCalculator.Models
+{
+    public class PremiumViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime DOB { get; set; }
+        public List<SelectListItem> OccupationList { get; set; }
+        public string Occupation { get; set; }
+        public int SumInsured { get; set; }
+        public double DeathPremium { get; set; }
+        public double TPDPremiumMonthly { get; set; }
+
+    }
+}
